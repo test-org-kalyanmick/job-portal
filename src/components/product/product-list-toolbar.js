a44e6e9fc305addc3e7eb13c0ee44809e4c102b8
Kalyan Chakravarthy S <kalyanmick@gmail.com>
2022-12-26 22:14:10 +0530

    Update product-list-toolbar.js

--- src/components/product/product-list-toolbar.js
+++ src/components/product/product-list-toolbar.js
@@ -27,7 +27,7 @@ export const ProductListToolbar = (props) => (
         sx={{ m: 1 }}
         variant="h4"
       >
-        Products
+        Job Opportunities
       </Typography>
       <Box sx={{ m: 1 }}>
         <Button
@@ -46,7 +46,7 @@ export const ProductListToolbar = (props) => (
           color="primary"
           variant="contained"
         >
-          Add products
+          Add Job
         </Button>
       </Box>
     </Box>
@@ -68,7 +68,7 @@ export const ProductListToolbar = (props) => (
                   </InputAdornment>
                 )
               }}
-              placeholder="Search product"
+              placeholder="Search jobs"
               variant="outlined"
             />
           </Box>
